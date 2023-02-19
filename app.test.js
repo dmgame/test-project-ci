753dee46bece4071001d6bb6525cf87cc10aa711
Denis Mescheryakov <denis.m.pcspace@gmail.com>
2023-02-19 17:53:35 +0100

    setup eslint for jest

--- app.test.js
+++ app.test.js
@@ -1,25 +1,25 @@
 const getRandomInt = require('./getRandomInt');
 
 test('getRandomInt generates a number less than the max', () => {
-    const max = 10;
-    const result = getRandomInt(max);
-    expect(result).toBeLessThan(max);
+  const max = 10;
+  const result = getRandomInt(max);
+  expect(result).toBeLessThan(max);
 });
 
 test('getRandomInt generates a number greater than or equal to 0', () => {
-    const max = 10;
-    const result = getRandomInt(max);
-    expect(result).toBeGreaterThanOrEqual(0);
+  const max = 10;
+  const result = getRandomInt(max);
+  expect(result).toBeGreaterThanOrEqual(0);
 });
 
 test('getRandomInt generates an integer', () => {
-    const max = 10;
-    const result = getRandomInt(max);
-    expect(result % 1).toBe(0);
+  const max = 10;
+  const result = getRandomInt(max);
+  expect(result % 1).toBe(0);
 });
 
 test('getRandomInt throws an error if max is not a number', () => {
-    expect(() => {
-        getRandomInt('not a number');
-    }).toThrow('max must be a number');
+  expect(() => {
+    getRandomInt('not a number');
+  }).toThrow('max must be a number');
 });

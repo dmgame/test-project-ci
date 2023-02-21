ccc0c2bb1285d3a95ce0d395f79af301db211c1c
Denis Mescheryakov <denis.m.pcspace@gmail.com>
2023-02-21 18:41:44 +0100

    fix tests

--- app.test.js
+++ app.test.js
@@ -17,9 +17,3 @@ test('getRandomInt generates an integer', () => {
   const result = getRandomInt(max);
   expect(result % 1).toBe(0);
 });
-
-test('getRandomInt throws an error if max is not a number', () => {
-  expect(() => {
-    getRandomInt('not a number');
-  }).toThrow('max must be a number');
-});
